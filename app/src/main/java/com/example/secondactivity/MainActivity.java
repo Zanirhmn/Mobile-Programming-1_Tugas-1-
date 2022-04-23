5be8a860ccbf18bde77af48d6e771062a8855899
Zanirhmn <zaniropirohman26@gmail.com>
2022-04-23 15:49:44 +0700

    update

--- app/src/main/java/com/example/secondactivity/MainActivity.java
+++ app/src/main/java/com/example/secondactivity/MainActivity.java
@@ -10,8 +10,23 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPindah;
-    EditText send;
-    String dikirim;
+    EditText nik;
+    EditText Nama;
+    EditText Tempat_Lahir;
+    EditText Tanggal_Lahir;
+    EditText Alamat;
+    EditText Jenis_Kelamin;
+    EditText Pekerjaan;
+    EditText Status_Perkawinan;
+
+    String _nik;
+    String _Nama;
+    String _Tempat_Lahir;
+    String _Tanggal_Lahir;
+    String _Alamat;
+    String _Jenis_Kelamin;
+    String _Pekerjaan;
+    String _Status_Perkawinan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +34,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnPindah = findViewById(R.id.buttonPindah);
-        send = findViewById(R.id.edit_sendvar);
+        nik = findViewById(R.id.nik);
+        Nama = findViewById(R.id.Nama);
+        Tempat_Lahir = findViewById(R.id.Tempat_Lahir);
+        Tanggal_Lahir = findViewById(R.id.Tanggal_Lahir);
+        Alamat = findViewById(R.id.Alamat);
+        Jenis_Kelamin = findViewById(R.id.Jenis_Kelamin);
+        Pekerjaan = findViewById(R.id.Pekerjaan);
+        Status_Perkawinan = findViewById(R.id.Status_Perkawinan);
 
         btnPindah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
-                dikirim = send.getText().toString();
-                pindah.putExtra("varFromMain", dikirim);
-                startActivity(pindah);
+                Intent nik_ = new Intent(MainActivity.this, SecondActivity.class);
+                Intent Nama_ = new Intent(MainActivity.this, SecondActivity.class);
+                Intent Tempat_Lahir_ = new Intent(MainActivity.this, SecondActivity.class);
+                Intent Tanggal_Lahir_ = new Intent(MainActivity.this, SecondActivity.class);
+                Intent Alamat_ = new Intent(MainActivity.this, SecondActivity.class);
+                Intent Jenis_Kelamin_ = new Intent(MainActivity.this, SecondActivity.class);
+                Intent Pekerjaan_ = new Intent(MainActivity.this, SecondActivity.class);
+                Intent Status_Perkawinana_ = new Intent(MainActivity.this, SecondActivity.class);
+
+                _nik = nik.getText().toString();
+                _Nama = Nama.getText().toString();
+                _Tempat_Lahir = Tempat_Lahir.getText().toString();
+                _Tanggal_Lahir = Tanggal_Lahir.getText().toString();
+                _Alamat = Alamat.getText().toString();
+                _Jenis_Kelamin = Jenis_Kelamin.getText().toString();
+                _Pekerjaan = Pekerjaan.getText().toString();
+                _Status_Perkawinan = Status_Perkawinan.getText().toString();
+                nik_.putExtra("nik", _nik);
+                Nama_.putExtra("Nama", _Nama);
+                Tempat_Lahir_.putExtra("Tempat_Lahir", _Tempat_Lahir);
+                Tanggal_Lahir_.putExtra("Tanggal_Lahir", _Tanggal_Lahir);
+                Alamat_.putExtra("Alamat", _Alamat);
+                Jenis_Kelamin_.putExtra("Jenis_Kelamin", _Jenis_Kelamin);
+                Pekerjaan_.putExtra("Pekerjaan", _Pekerjaan);
+                Status_Perkawinana_.putExtra("Status_Perkawinan", _Status_Perkawinan);
+                startActivity(nik_);
+                startActivity(Nama_);
+                startActivity(Nama_);
+                startActivity(Nama_);
+                startActivity(Nama_);
+                startActivity(Nama_);
+                startActivity(Nama_);
+                startActivity(Nama_);
+
             }
         });
     }
--- app/src/main/java/com/example/secondactivity/SecondActivity.java
+++ app/src/main/java/com/example/secondactivity/SecondActivity.java
@@ -6,18 +6,60 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
- TextView txthello;
- private String ACC_VARIABLE;
+ TextView _nik;
+    TextView _Nama;
+    TextView _Tempat_Lahir;
+    TextView _Tanggal_Lahir;
+    TextView _Alamat;
+    TextView _Jenis_Kelamin;
+    TextView _Pekerjaan;
+    TextView _Status_Perkawinan;
+
+
+
+    private String nik;
+    private String Nama;
+    private String Tempat_Lahir;
+    private String Tanggal_Lahir;
+    private String Alamat;
+    private String Jenis_Kelamin;
+    private String Pekerjaan;
+    private String Status_Perkawinan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        txthello = (TextView) findViewById(R.id.text_accvar);
+        _nik = (TextView) findViewById(R.id.nik);
+        _Nama = (TextView) findViewById(R.id.Nama);
+        _Tempat_Lahir = (TextView) findViewById(R.id.Tempat_Lahir);
+        _Tanggal_Lahir = (TextView) findViewById(R.id.Tanggal_Lahir);
+        _Alamat = (TextView) findViewById(R.id.Alamat);
+        _Jenis_Kelamin = (TextView) findViewById(R.id.Jenis_Kelamin);
+        _Pekerjaan = (TextView) findViewById(R.id.Pekerjaan);
+        _Status_Perkawinan = (TextView) findViewById(R.id.Status_Perkawinan);
+
 
         Bundle extras = getIntent().getExtras();
-        ACC_VARIABLE = extras.getString("varFromMain");
-        txthello.setText("Hallo, "+ ACC_VARIABLE+ "!");
+        nik = extras.getString("nik");
+        Nama = extras.getString("Nama");
+        Tempat_Lahir = extras.getString("Tempat_Lahir");
+        Tanggal_Lahir = extras.getString("Tanggal_Lahir");
+        Alamat = extras.getString("Alamat");
+        Jenis_Kelamin = extras.getString("Jenis_Kelamin");
+        Pekerjaan = extras.getString("Pekerjaan");
+        Status_Perkawinan = extras.getString("Status_Perkawinan");
+
+        _nik.setText("NIK : "+ nik+ "!");
+        _Nama.setText("NIK : "+ Nama+ "!");
+        _Tempat_Lahir.setText("NIK : "+ Tempat_Lahir+ "!");
+        _Tanggal_Lahir.setText("NIK : "+ Tanggal_Lahir+ "!");
+        _Alamat.setText("NIK : "+ Alamat+ "!");
+        _Jenis_Kelamin.setText("NIK : "+ Jenis_Kelamin+ "!");
+        _Pekerjaan.setText("NIK : "+ Pekerjaan+ "!");
+        _Status_Perkawinan.setText("NIK : "+ Status_Perkawinan+ "!");
+
     }
 }
